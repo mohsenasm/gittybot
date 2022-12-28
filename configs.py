c84790e5bcd62004fde5bc7798364edc6e3278a8
Mohammad-Mohsen Aseman-Manzar <m.m.asemanmanzar@gmail.com>
2022-12-28 09:29:24 +0330

    remove heroku backward compatibility

--- configs.py
+++ configs.py
@@ -9,7 +9,6 @@ BOT_ADMIN_ID = os.environ['BOT_ADMIN_ID']
 LOG_VIEWER_USERNAME = os.environ['LOG_VIEWER_USERNAME']
 LOG_VIEWER_PASSWORD = os.environ['LOG_VIEWER_PASSWORD']
 KEY = os.environ['KEY']
-HEROKU_KEY = os.environ['HEROKU_KEY'] # for backward compatibility. will remove soon.
 PORT = os.environ['PORT']
 WEBHOOK_BASE_URL = os.environ['WEBHOOK_BASE_URL']
 
